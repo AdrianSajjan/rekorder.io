7462eb2c2f132d5cd8b9775116e0805a1875ce6d
adriansajjan <adriansajjan2001@gmail.com>
2024-12-28 22:18:25 +0530

    minor - fixes

--- apps/recorder-e2e/src/support/commands.ts
+++ apps/recorder-e2e/src/support/commands.ts
@@ -35,3 +35,5 @@ Cypress.Commands.add('login', (email, password) => {
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+export {};
