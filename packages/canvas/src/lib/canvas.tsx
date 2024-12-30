916ee8abfb8368271c7d3b82f0600899324650e4
adriansajjan <adriansajjan2001@gmail.com>
2024-12-31 02:40:48 +0530

    Refactor Jest configuration and update TypeScript paths
    - Removed Jest configuration files (`jest.config.ts` and `jest.preset.js`) as part of the transition to Vite for testing.
    - Updated `nx.json` to reflect changes in project structure and inputs.
    - Added new path mapping for `@rekorder.io/canvas` in `tsconfig.base.json` to improve module resolution.
    - Cleaned up TypeScript configuration files in the `recorder` app to remove references to Jest and include Vite configurations.
    - Introduced Vite test configuration in `vite.config.ts` for improved testing capabilities.
    
    These changes streamline the testing setup and enhance the organization of the codebase, facilitating a smoother development experience.

---
+++ packages/canvas/src/lib/canvas.tsx
@@ -0,0 +1,14 @@
+export function Canvas() {
+  return (
+    <div>
+      <style jsx>{`
+        div {
+          color: pink;
+        }
+      `}</style>{' '}
+      <h1>Welcome to Canvas!</h1>
+    </div>
+  );
+}
+
+export default Canvas;
--- packages/canvas/vite.config.ts
+++ packages/canvas/vite.config.ts
@@ -1,47 +1,31 @@
-/// <reference types='vitest' />
-import { defineConfig } from 'vite';
+import * as path from 'path';
+
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+
+import { defineConfig } from 'vite';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/recorder',
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-  define: {
-    'process.env': {},
-  },
-  plugins: [
-    react({
-      babel: {
-        plugins: ['styled-jsx/babel'],
-      },
-    }),
-    nxViteTsPaths(),
-    nxCopyAssetsPlugin(['*.md']),
-  ],
+  cacheDir: '../../node_modules/.vite/packages/canvas',
+  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md']), dts({ entryRoot: 'src', tsconfigPath: path.join(__dirname, 'tsconfig.lib.json') })],
   build: {
-    outDir: '../../dist/apps/recorder',
+    outDir: '../../dist/packages/canvas',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
       transformMixedEsModules: true,
     },
     lib: {
-      entry: 'src/main.tsx',
-      name: 'recorder',
+      entry: 'src/index.ts',
+      name: 'canvas',
+      fileName: 'index',
+      formats: ['es'],
     },
     rollupOptions: {
-      output: {
-        entryFileNames: `content-script.js`,
-      },
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
     },
   },
   test: {
@@ -51,7 +35,7 @@ export default defineConfig({
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
     coverage: {
-      reportsDirectory: '../../coverage/apps/recorder',
+      reportsDirectory: '../../coverage/packages/canvas',
       provider: 'v8',
     },
   },
