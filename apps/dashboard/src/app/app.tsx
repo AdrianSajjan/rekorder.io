c038b2e40155727834f78a553544a7554ad487a8
adriansajjan <adriansajjan2001@gmail.com>
2025-01-05 19:38:58 +0530

    todo: start with the saas dashboard

---
+++ apps/dashboard/src/app/app.tsx
@@ -0,0 +1,14 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <style jsx>{`
+        /** your style here **/
+      `}</style>
+      <NxWelcome title="dashboard" />
+    </div>
+  );
+}
+
+export default App;
